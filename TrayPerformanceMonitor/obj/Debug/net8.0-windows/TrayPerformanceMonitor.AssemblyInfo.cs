4442ccb9ecaf75cf39e979667abb312abb8f852a
Jeter Pontes <jeter.pontes@outlook.com>
2026-02-02 13:45:15 -0800

    updated readme and system prompt, and ensure ai analysis also get pruned

--- TrayPerformanceMonitor/obj/Debug/net8.0-windows/TrayPerformanceMonitor.AssemblyInfo.cs
+++ TrayPerformanceMonitor/obj/Debug/net8.0-windows/TrayPerformanceMonitor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright © 2024-2026 TrayPerformanceMonitor")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ebf4d4fe6646ecc9608b57d0b63eceaa717d6d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fda05529d4ad74e224853de933d7903224d2f3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrayPerformanceMonitor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrayPerformanceMonitor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
